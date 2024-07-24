84fd737a734eba27eda47e20e58a7f25a08a4a35
Jake Zeledon <jackiezeledon@gmail.com>
2024-07-24 15:33:28 -0600

    Add api service

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private apiUrl = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<any> {
+    return this.http.get(`${this.apiUrl}/posts`);
+  }
+
+  getPost(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/posts/${id}`);
+  }
+
+  getFavorites(userEmail: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/favorites?userEmail=${userEmail}`);
+  }
+
+  addFavorite(favorite: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/favorites`, favorite);
+  }
+
+  removeFavorite(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/favorites/${id}`);
+  }
+
+  createPost(post: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/posts`, post);
+  }
+
+  editPost(id: number, post: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/posts/${id}`, post);
+  }
+}
