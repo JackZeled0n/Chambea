5d8dfd83eaa4b67b537b4857d3dc530b6737ff97
Jake Zeledon <jackiezeledon@gmail.com>
2024-07-25 18:11:22 -0600

    Call service for loading user data

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import { ApiService } from '../../../core/services/api.service';
 
 @Component({
   selector: 'app-card',
@@ -9,15 +10,17 @@ import { Router } from '@angular/router';
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   @Input() title: string = '';
   @Input() summary: string = '';
   @Input() imageUrl: string = '';
-  @Input() author: string = '';
+  @Input() author: string = ''; 
   @Input() date: string = '';
   @Input() id: string = '';
+  userName: string = ''; 
+  avatar: string = '';
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private apiService: ApiService) {}
 
   isFavorite: boolean = false;
 
@@ -31,5 +34,27 @@ export class CardComponent {
 
   onSeePostClick() {
     this.router.navigate(['/post-details', this.id]);
+  }
+
+  loadUserByEmail(email: string) {
+    this.apiService.getUserByEmail(email).subscribe({
+      next: (users) => {
+        if (users && users.length > 0) {
+          this.userName = users[0].name;
+          this.avatar = users[0].avatarUrl;
+        } else {
+          this.userName = 'Unknown Author';
+          this.avatar = './assets/img/empty-user.png'
+        }
+      },
+      error: (error) => {
+        console.error('Error loading user name:', error);
+        this.userName = 'Unknown Author';
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.loadUserByEmail(this.author);
   }
 }
