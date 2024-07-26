ed72279dda8d4415af9c3290b4eb83004992ef70
Jake Zeledon <jackiezeledon@gmail.com>
2024-07-25 18:38:57 -0600

    Call auth modal component

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AuthModalComponent } from '../auth-modal/auth-modal.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [AuthModalComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit  {
+  @ViewChild(AuthModalComponent) authModal!: AuthModalComponent;
 
+  ngAfterViewInit() {
+    console.log(this.authModal);
+  }
+
+  openLoginModal() {
+    this.authModal.openModal();
+  }
 }
+
