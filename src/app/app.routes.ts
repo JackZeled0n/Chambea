4c84f2504607b3dacd451306406f90464ae2afff
Jake Zeledon <jackiezeledon@gmail.com>
2024-07-26 12:49:42 -0600

    Add to routes the create edit post component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { PostDetailsComponent } from './features/post-details/components/post-details.component';
 import { FavoritesComponent } from './features/favorites/favorites.component';
 import { AuthGuard } from './core/services/auth.guard';
+import { CreateEditPostComponent } from './features/create-edit-post/create-edit-post.component';
 
 export const routes: Routes = [
     { path: '', loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule) },
     { path: 'post-details/:id', component: PostDetailsComponent },
-    { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard] }
+    { path: 'favorites', component: FavoritesComponent, canActivate: [AuthGuard] },
+    { path: 'create-post', component: CreateEditPostComponent, canActivate: [AuthGuard] },
+    { path: 'edit-post/:id', component: CreateEditPostComponent, canActivate: [AuthGuard] },
 ];
